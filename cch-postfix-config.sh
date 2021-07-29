cf5e31a0885fcac97abbe4ecf617ae507341e3b4
jevans3 <jevans3@users.noreply.github.com>
2021-07-29 13:19:18 -0400

    add CCH Dockerfile
    added cch dockerfile, postfix config and a simple test mail script

---
+++ cch-postfix-config.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+postconf -e "relayhost = email-smtp.us-east-1.amazonaws.com:587"
+postconf -e "smtp_sasl_auth_enable = yes"
+postconf -e "smtp_sasl_security_options = noanonymous"
+postconf -e "smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd"
+postconf -e "smtp_use_tls = yes"
+postconf -e "smtp_tls_security_level = encrypt"
+postconf -e "smtp_tls_note_starttls_offer = yes"
---
+++ test-mail.sh
@@ -0,0 +1,5 @@
+sendmail -f jesse.evans@infor.com jesse.evans@infor.com
+From: Jesse Evans<jesse.evans@infor.com>
+Subject: Amazon SES Test- From jesse evans                
+This message was sent using Amazon SES.                
+.
